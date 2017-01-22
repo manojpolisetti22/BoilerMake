25564e0ab0273293f06ff2c419badf166b61800c
Niki Markley <Niki Markley>
2017-01-22 01:22:10 -0500

    Changed server to use express and host html file

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+var path = require('path');
+
 
 const PORT=8080;
 
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/template.html'));
+});
+
+app.listen(8080);
+
+
+/*
 function handleRequest(request, response){
 	response.end('It works!! Path Hit: ' + request.url);
 }
@@ -11,3 +24,4 @@ var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
 	console.log("Server listening on: http://localhost:%s", PORT);
 });
+*/

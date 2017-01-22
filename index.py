3c9a447f876f1f32bcb6196cdd0dbe01712ff8ab
manojpolisetti22 <manojpolisetti22@gmail.com>
2017-01-22 02:28:32 -0500

    Change to the actual Python webscraping function

--- index.py
+++ index.py
@@ -2,39 +2,36 @@ import webbrowser
 from lxml import html
 import requests
 from bs4 import BeautifulSoup
-import wikipedia
 import re
 
+def getBooks(s):
+	
+	firstName,lastName = s.split(" ")
+	page = requests.get('https://en.wikipedia.org/wiki/'+firstName+'_'+lastName)
+	url = page.text
 
-firstName,lastName = input('Enter the author: ').split(" ")
-#lastList = list(lastName)
+	soup = BeautifulSoup(url, 'lxml')
 
-
-page = requests.get('https://en.wikipedia.org/wiki/'+firstName+'_'+lastName)
-url = page.text
-#webbrowser.open('https://en.wikipedia.org/wiki/'+firstName+'_'+lastName, new=2)
-
-soup = BeautifulSoup(url, 'lxml')
-
-biblio = soup.find('span', {'id':'Bibliography'})
-#print (biblio)
-h2 = biblio.parent
-h3 = h2.findNextSiblings('ol')
-h3.extend(h2.findNextSiblings('ul'))
+	biblio = soup.find('span', {'id':'Bibliography'})
+	h2 = biblio.parent
+	h3 = h2.findNextSiblings('ol')
+	h3.extend(h2.findNextSiblings('ul'))
 
 
-empty = []
+	empty = []
+	
+	for li in h3:
+		obj = list(li.find_all('li'))
+		for str in obj:
+			if "2017" in str.text:
+				empty.append(str.text)
+			elif "2018" in str.text:
+				empty.append(str.txt)
+				#print (str.text)
+			elif "2019" in str.text:
+				empty.append(str.text)
 
-for li in h3:
-	obj = list(li.find_all('li'))
-	for str in obj:
-		#searchObj = re.search(r'/*[2017-9]*/', str.text)
-		if "2017" in str.text:
-			empty.append(str.text)
-		elif "2018" in str.text:
-			empty.append(str.txt)
-			print (str.text)
-		elif "2019" in str.text:
-			empty.append(str.text)
+	print(empty)
 
-print(empty)
+
+#getBooks(input('Enter Author: '))
